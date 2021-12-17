1589f59dc7aab98fb4034ce266b9b55f96188b02
Jair Carbajal <jair.carbajal91@gmail.com>
2021-12-17 12:28:05 -0800

    Will practice regex further later on

---
+++ regex/regex.js
@@ -0,0 +1,11 @@
+// What are Regular Expressions?
+// In JS, Regular Expressions are objects that describes a pattern of characters. Used to match a character combination in a string and perform matching and search and replace text.
+// Real life application of Regex is email or phone number validations. As well as password or domain name. Making sure that we have the right length or characters.
+
+
+/** SYNTAX for Regex
+  /pattern/modifiers;
+  var alphabet = /ou/i;
+  pattern => ou
+  modifier => i(case insensitive)
+*/
