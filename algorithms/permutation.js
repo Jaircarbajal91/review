61adfa7da4b035ce50c8038d83865d6720cf55cd
Jair Carbajal <jair.carbajal91@gmail.com>
2021-12-23 09:26:26 -0800

    Updated module

--- algorithms/permutation.js
+++ algorithms/permutation.js
@@ -44,9 +44,8 @@ let permutations = function (str) {
   }
 }
 
-
 function isPermutation(str1, str2) {
   return str1.split('').sort().join('') == str2.split('').sort().join('');
-}
+};
 
-console.log(isPermutation('india', 'ndiia'))
+console.log(isPermutation('india', 'ndiia'));
