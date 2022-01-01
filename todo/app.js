2c88e4021a653fa350f5805b0737209d074fa67d
Jair Carbajal <jair.carbajal91@gmail.com>
2021-12-31 18:35:51 -0800

    App now works with localstorage

--- todo/app.js
+++ todo/app.js
@@ -4,35 +4,47 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const todoList = document.querySelector('ul')
   const button = document.querySelector('button')
 
+  const todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
+  localStorage.setItem('todos', JSON.stringify(todosArray))
+
+  const storage = JSON.parse(localStorage.getItem('todos'))
 
   form.addEventListener('submit', (e) => {
     e.preventDefault();
+    todosArray.push(input.value)
+    localStorage.setItem('todos', JSON.stringify(todosArray))
     toDoMaker(input.value);
     input.value = '';
+    console.log(localStorage)
   })
 
   // makes li tags and appends them to todoList
   const toDoMaker = (text) => {
     let todo = document.createElement('li');
     todo.textContent = text;
-    let radioButton = createCheckMarkElement('todo-radio', false);
-    todo.appendChild(radioButton);
+    // let radioButton = createCheckMarkElement('todo-radio', false);
+    // todo.appendChild(radioButton);
     todoList.appendChild(todo)
   }
 
-  const createCheckMarkElement = (name, checked) => {
-    var radioHtml = '<input type="checkbox" name="' + name + '"';
-    if (checked) {
-      radioHtml += ' checked="checked"';
-    }
-    radioHtml += '/>';
-    var radioFragment = document.createElement('div');
-    radioFragment.innerHTML = radioHtml;
-    return radioFragment.firstChild
+  for (let todo of storage) {
+    toDoMaker(todo)
   }
+
+  // const createCheckMarkElement = (name, checked) => {
+  //   var radioHtml = '<input type="checkbox" name="' + name + '"';
+  //   if (checked) {
+  //     radioHtml += ' checked="checked"';
+  //   }
+  //   radioHtml += '/>';
+  //   var radioFragment = document.createElement('div');
+  //   radioFragment.innerHTML = radioHtml;
+  //   return radioFragment.firstChild
+  // }
 
 
   button.addEventListener('click', (e) => {
+    localStorage.clear()
     while (todoList.hasChildNodes()) {
       todoList.removeChild(todoList.firstChild);
     }
