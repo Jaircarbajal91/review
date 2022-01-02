74531748e960965492769e5bb4e440e2cc0bfe92
Jair Carbajal <jair.carbajal91@gmail.com>
2022-01-02 11:27:37 -0800

    You can now remove a specific item by clicking checkbox

--- todo/app.js
+++ todo/app.js
@@ -3,6 +3,8 @@ document.addEventListener('DOMContentLoaded', (event) => {
   const input = document.getElementById('user-todo')
   const todoList = document.querySelector('ul')
   const button = document.querySelector('button')
+  const checkbox = document.querySelector("input[name=checkbox]");
+
 
   const todosArray = localStorage.getItem('todos') ? JSON.parse(localStorage.getItem('todos')) : [];
   localStorage.setItem('todos', JSON.stringify(todosArray))
@@ -18,30 +20,40 @@ document.addEventListener('DOMContentLoaded', (event) => {
     console.log(localStorage)
   })
 
+  const removeItemFromLocalStorage = (value) => {
+    const index = todosArray.indexOf(value)
+    todosArray.splice(index, 1)
+    localStorage.setItem('todos', JSON.stringify(todosArray))
+  }
+
+  const createCheckMarkElement = (name, checked) => {
+    var radioHtml = '<input type="checkbox" name="' + name + '"';
+    if (checked) {
+      radioHtml += ' checked="checked"';
+    }
+    radioHtml += '/>';
+    var radioFragment = document.createElement('div');
+    radioFragment.innerHTML = radioHtml;
+    return radioFragment.firstChild
+  }
+
   // makes li tags and appends them to todoList
   const toDoMaker = (text) => {
     let todo = document.createElement('li');
     todo.textContent = text;
-    // let radioButton = createCheckMarkElement('todo-radio', false);
-    // todo.appendChild(radioButton);
+    let radioButton = createCheckMarkElement('todo-radio', false);
+    radioButton.addEventListener('change', (e) => {
+      removeItemFromLocalStorage(todo.textContent)
+      // console.log(todo.textContent)
+      todo.remove();
+    })
+    todo.appendChild(radioButton);
     todoList.appendChild(todo)
   }
 
   for (let todo of storage) {
     toDoMaker(todo)
   }
-
-  // const createCheckMarkElement = (name, checked) => {
-  //   var radioHtml = '<input type="checkbox" name="' + name + '"';
-  //   if (checked) {
-  //     radioHtml += ' checked="checked"';
-  //   }
-  //   radioHtml += '/>';
-  //   var radioFragment = document.createElement('div');
-  //   radioFragment.innerHTML = radioHtml;
-  //   return radioFragment.firstChild
-  // }
-
 
   button.addEventListener('click', (e) => {
     localStorage.clear()
